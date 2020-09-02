3afc0591acbb8e63b8f6e605949de2e241171fd5
Brendan Allen <99brendanallen@gmail.com>
2020-09-02 00:26:01 -0400

    hw1
    hw1 as of Sept 2 at 12:26am

---
+++ hw1.cpp
@@ -0,0 +1,46 @@
+/*
+   Program 1:  Find Stats
+   Programmer: Brendan Allen
+   Class:  CSCI 230,  Fall 2020
+   Short Description:  This program
+
+   Narrative:
+        This program ...
+
+   Special Notes:
+      *  If you run this program with the -G flag, the program will
+	 simulate the presence of God, and thus no missing link will be
+	 found, man is just created.
+      *  This program ...
+*/
+
+#include <iostream>
+#include <iomanip>
+
+
+
+using namespace std;
+
+void EnterMasterFile();
+
+const string MASTER_FILE = "master.list";
+
+int main() {
+
+  // open master file
+  EnterMasterFile();
+
+
+  return 0;
+}
+
+void EnterMasterFile() {
+  string masterFile;
+
+  // enter in master file
+  cout << "Enter the name of the master file => "<<endl;
+  cin >> masterFile;
+  if (masterFile != MASTER_FILE) {
+    cout << "ERROR: Unable to open the master file " << masterFile << "." << endl;
+  }
+}
